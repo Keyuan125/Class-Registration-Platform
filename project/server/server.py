004a494d763b5de8262a45430d79c2b173323ddc
Yiyan Wang <yiyanw3@yongjoo-cs411-course-project.q5uqgrs2d4ye5bdt0ts4tjiexf.ex.internal.cloudapp.net>
2021-11-30 22:21:28 +0000

    move helper function to utility

--- project/server/server.py
+++ project/server/server.py
@@ -4,6 +4,9 @@ import re
 import pandas as pd
 import numpy as np
 from fuzzywuzzy import fuzz
+from utility.key_word_search import key_word_search
+from utility.prereq import prereq
+
 app = Flask(__name__) 
 
 mydb = mysql.connector.connect(
@@ -12,202 +15,11 @@ mydb = mysql.connector.connect(
             database='yiyanw3_database',
             password='!1234QWERasdf')
 
+
 @app.route('/', methods=['GET'])
 def loginPage():
     return render_template('enroll.html', search=None, enrollment=None)
 
-# # http://20.88.14.242:10056/delete query_string drop table Test
-# @app.route("/delete", methods = ['POST'])
-# def deleteSth():
-#     sql = request.form['query_string']
-#     if sql.split()[0].upper() != "DELETE" and sql.split()[0].upper() != "DROP":
-#         abort(400, "must be create")
-    
-#     try:
-#         mycursor = mydb.cursor() 
-#         mycursor.execute(sql)
-#         mydb.commit()
-#     except:
-#         abort(400, "fail to create table") 
-
-#     return 'success'
-
-# # postman: http://20.88.14.242:10056/create  key:query_string value:create table acc ( number   char(3))
-# @app.route("/create", methods = ['POST'])
-# def newTable():
-#     sql = request.form['query_string']
-#     if sql.split()[0].upper() != "CREATE":
-#         abort(400, "must be create")
-    
-#     try:
-#         mycursor = mydb.cursor() 
-#         mycursor.execute(sql)
-#         mydb.commit()
-#     except:
-#         abort(400, "fail to create table") 
-
-#     return 'success'
-
-# # http://20.88.14.242:10056/insert key:query_string value:insert into account values (101, 'J. Smith', 1000.00, 'checking')
-# @app.route("/insert", methods = ['POST'])
-# def insertRecord():
-#     sql = request.form['query_string']
-#     print(sql)
-#     if sql.split()[0].upper() != "INSERT":
-#         abort(400, "must be insert")
-    
-#     try:
-#         mycursor = mydb.cursor() 
-#         mycursor.execute("SET FOREIGN_KEY_CHECKS=0;")
-#         mycursor.execute(sql)
-#         mydb.commit()
-#     except:
-#         abort(400, "fail to insert record") 
-
-#     return 'success'
-
-
-# @app.route("/update", methods = ['POST'])
-# def updateRecord():
-#     sql = request.form['query_string']
-#     print(sql)
-    
-#     try:
-#         mycursor = mydb.cursor()
-#         mycursor.execute(sql)
-#         mydb.commit()
-#     except:
-#         abort(400, "fail to update") 
-
-#     return 'success'
-
-# # http://20.88.14.242:10056/select key:query_string values:select * from account
-# @app.route("/select", methods = ['GET'])
-# def showRecord():
-#     sql = request.form['query_string']
-    
-#     if sql.split()[0].upper() != "SELECT":
-#         abort(400, "must be select")
-    
-#     try:
-#         mycursor = mydb.cursor() 
-#         mycursor.execute(sql)
-#     except:
-#         abort(400, "fail to get record") 
-
-#     # print([type(row[0]) for row in mycursor.description]) 
-#     # print([row for row in mycursor])
-#     result = {}
-#     result['query_string'] = sql
-#     result['data'] = {}
-#     result['data']['labels'] = [row[0] for row in mycursor.description]
-#     # print([type(row) for row in mycursor])
-#     result['data']['values'] = [row for row in mycursor]
-#     result['data']['values']
-#     return result
-
-
-def key_word_search(result, key_word):
-    '''
-    input:
-    df: pandas dataframe, contain all courses infomation
-    key_word: string
-
-    return: 
-    df_out: pandas dataframe, contain all courses info relate to key word
-    '''
-
-    labels = result['labels']
-    values = result['values']
-    new_values = None
-    df = pd.DataFrame(values, columns=labels)
-    if key_word.isalpha():
-        # subject search
-        if len(key_word) >=2 and len(key_word) <= 4:
-            sub = key_word.upper()
-            df_sub = df[df['Subject'] == sub]
-            if len(df_sub) > 0:
-                df_out = df_sub
-                records = df_out.to_records(index=False)
-                new_values = list(records)
-                if len(new_values) > 10:
-                    new_values = new_values[:10]
-                new_values.sort(key=lambda course: course[5])
-                return new_values
-                
-    else:
-        ints = re.findall("\d", key_word)
-        alphas = re.findall('[A-Za-z]', key_word)
-        if len(ints) == 3 and len(alphas) > 0:
-            num = int(ints[0] + ints[1] + ints[2])
-            df_num = df[df['Number'] == num]
-            if len(df_num) == 0:
-                pass
-            else: 
-                if len(alphas) <=4 and len(alphas) >= 2:
-                    sub = ''.join(alphas)
-                    sub = sub.upper()
-                    df_sub_num = df_num[df_num['Subject'] == sub]
-                    if len(df_sub_num) == 0:
-                        pass
-                    else:
-                        df_out = df_sub_num
-                        records = df_out.to_records(index=False)
-                        new_values = list(records)
-                        if len(new_values) > 10:
-                            new_values = new_values[:10]
-                        new_values.sort(key=lambda course: course[5])
-                        return new_values
-
-
-        elif len(ints) == 1 and len(alphas) > 0:
-            num = int(ints[0])
-            df_num = df[df['Number'] // 100 == num]
-            if len(df_num) == 0:
-                pass
-            else: 
-                if len(alphas) <=4 and len(alphas) >= 2:
-                    sub = ''.join(alphas)
-                    sub = sub.upper()
-                    df_sub_num = df_num[df_num['Subject'] == sub]
-                    if len(df_sub_num) == 0:
-                        pass
-                    else:
-                        df_out = df_sub_num
-                        records = df_out.to_records(index=False)
-                        new_values = list(records)
-                        if len(new_values) > 10:
-                            new_values = new_values[:10]
-                        new_values.sort(key=lambda course: course[5])
-                        return new_values
-        
-        elif len(ints) == 3 and len(alphas) == 0:
-            num = int(ints[0] + ints[1] + ints[2])
-            df_num = df[df['Number'] == num]
-            if len(df_num) == 0:
-                pass
-            else:
-                df_out = df_num
-                records = df_out.to_records(index=False)
-                new_values = list(records)
-                if len(new_values) > 10:
-                    new_values = new_values[:10]
-                new_values.sort(key=lambda course: course[5])
-                return new_values
-        else:
-            pass
-
-    df_apply = df.apply(
-        lambda row: fuzz.partial_ratio(row['Name'], key_word), axis=1
-    )
-    df_out = df[df_apply > 70]
-    records = df_out.to_records(index=False)
-    new_values = list(records)
-    
-    if len(new_values) > 10:
-        new_values = new_values[:10]
-
-    return new_values
 
 
 @app.route("/easy", methods = ['GET', 'POST'])
---
+++ project/server/utility/key_word_search.py
@@ -0,0 +1,101 @@
+def key_word_search(result, key_word):
+    '''
+    input:
+    df: pandas dataframe, contain all courses infomation
+    key_word: string
+
+    return: 
+    df_out: pandas dataframe, contain all courses info relate to key word
+    '''
+
+    labels = result['labels']
+    values = result['values']
+    new_values = None
+    df = pd.DataFrame(values, columns=labels)
+    if key_word.isalpha():
+        # subject search
+        if len(key_word) >=2 and len(key_word) <= 4:
+            sub = key_word.upper()
+            df_sub = df[df['Subject'] == sub]
+            if len(df_sub) > 0:
+                df_out = df_sub
+                records = df_out.to_records(index=False)
+                new_values = list(records)
+                if len(new_values) > 10:
+                    new_values = new_values[:10]
+                new_values.sort(key=lambda course: course[5])
+                return new_values
+                
+    else:
+        ints = re.findall("\d", key_word)
+        alphas = re.findall('[A-Za-z]', key_word)
+        if len(ints) == 3 and len(alphas) > 0:
+            num = int(ints[0] + ints[1] + ints[2])
+            df_num = df[df['Number'] == num]
+            if len(df_num) == 0:
+                pass
+            else: 
+                if len(alphas) <=4 and len(alphas) >= 2:
+                    sub = ''.join(alphas)
+                    sub = sub.upper()
+                    df_sub_num = df_num[df_num['Subject'] == sub]
+                    if len(df_sub_num) == 0:
+                        pass
+                    else:
+                        df_out = df_sub_num
+                        records = df_out.to_records(index=False)
+                        new_values = list(records)
+                        if len(new_values) > 10:
+                            new_values = new_values[:10]
+                        new_values.sort(key=lambda course: course[5])
+                        return new_values
+
+
+        elif len(ints) == 1 and len(alphas) > 0:
+            num = int(ints[0])
+            df_num = df[df['Number'] // 100 == num]
+            if len(df_num) == 0:
+                pass
+            else: 
+                if len(alphas) <=4 and len(alphas) >= 2:
+                    sub = ''.join(alphas)
+                    sub = sub.upper()
+                    df_sub_num = df_num[df_num['Subject'] == sub]
+                    if len(df_sub_num) == 0:
+                        pass
+                    else:
+                        df_out = df_sub_num
+                        records = df_out.to_records(index=False)
+                        new_values = list(records)
+                        if len(new_values) > 10:
+                            new_values = new_values[:10]
+                        new_values.sort(key=lambda course: course[5])
+                        return new_values
+        
+        elif len(ints) == 3 and len(alphas) == 0:
+            num = int(ints[0] + ints[1] + ints[2])
+            df_num = df[df['Number'] == num]
+            if len(df_num) == 0:
+                pass
+            else:
+                df_out = df_num
+                records = df_out.to_records(index=False)
+                new_values = list(records)
+                if len(new_values) > 10:
+                    new_values = new_values[:10]
+                new_values.sort(key=lambda course: course[5])
+                return new_values
+        else:
+            pass
+
+    df_apply = df.apply(
+        lambda row: fuzz.partial_ratio(row['Name'], key_word), axis=1
+    )
+    df_out = df[df_apply > 70]
+    records = df_out.to_records(index=False)
+    new_values = list(records)
+    
+    if len(new_values) > 10:
+        new_values = new_values[:10]
+
+    return new_values
